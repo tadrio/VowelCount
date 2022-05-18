11859a7953078ca42f92f2334a5c7ccd5e8b752b
Tadrio Tabosa <tadrio_88@hotmail.com>
2022-05-18 14:41:07 -0300

    Improvements
    Now the code accepts CAPITAL LETTERS, and has been refactored

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const phrase = "o rato roeu a roupa do rei de roma";
+const phrase = "O Rato roeu A roupa do rei de roma";
+const vogals = 'aAeEiIoOuU';
 
 let count = 0;
 
 function vowelsCount() {
   for (let i = 0; i < phrase.length; i++) {
-    if (
-      phrase[i] === "a" ||
-      phrase[i] === "e" ||
-      phrase[i] === "i" ||
-      phrase[i] === "o" ||
-      phrase[i] === "u"
-    )
-      count++;
+    for (let j = 0; j < vogals.length; j++) {
+      if (phrase[i] === vogals[j]){
+        count++;
+      }
+    }
   }
   return count;
 }
 
-console.log(vowelsCount());
+console.log(`The phrase "${phrase}" have ${vowelsCount()} vowels`);
